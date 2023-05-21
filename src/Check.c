fec8b87e81020ea050fe301f1d5ff67c2740886e
cybersanta <cybersanta9@ya.ru>
2023-05-22 04:44:59 +0700

    Minor fixes

--- src/Check.c
+++ src/Check.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
+п»ї#include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
-#ifndef CHECK_H
-#define CHECK_H
-bool check_file_exists(const char* filename);
-bool check_num_vertices(const char* filename);
-bool check_matrix(const char* filename);
-bool check_matrix_element(int x, int row, int col);
 
+#ifndef CHECK_C
+#define CHECK_C
 
-
-bool check_file_exists(const char* filename)
+bool check_file_exists(const char *filename)
 {
-    FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
         printf("Couldn't open file for reading\n");
         return false;
     }
@@ -21,40 +17,47 @@ bool check_file_exists(const char* filename)
     return true;
 }
 
-bool check_num_vertices(const char* filename)
+bool check_num_vertices(const char *filename)
 {
-    FILE* fp = fopen(filename, "r");
+    FILE *fp = fopen(filename, "r");
     int n;
-    if (fscanf(fp, "%d", &n) != 1) {
-        printf("Couldn't read number of vertices\n"); //Check int N
+    if (fscanf(fp, "%d", &n) != 1)
+    {
+        printf("Couldn't read number of vertices\n");
         fclose(fp);
         return false;
     }
     fclose(fp);
-    if (n < 0) {
-        printf("Invalid number of vertices\n");// Check -N
+    if (n < 0)
+    {
+        printf("Invalid number of vertices\n");
         return false;
     }
     return true;
 }
 
-bool check_matrix(const char* filename)
+bool check_matrix(const char *filename)
 {
-    FILE* fp = fopen(filename, "r");
+    FILE *fp = fopen(filename, "r");
     int n;
-    if (fscanf(fp, "%d", &n) != 1) {
+    if (fscanf(fp, "%d", &n) != 1)
+    {
         fclose(fp);
         return false;
     }
     int x;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (fscanf(fp, "%d", &x) != 1) {
-                printf("Couldn't read adjacency matrix element at position (%d, %d)\n", i+1, j+1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (fscanf(fp, "%d", &x) != 1)
+            {
+                printf("Couldn't read adjacency matrix element at position (%d, %d)\n", i + 1, j + 1);
                 fclose(fp);
                 return false;
             }
-            if (!check_matrix_element(x, i+1, j+1)) {
+            if (!check_matrix_element(x, i + 1, j + 1))
+            {
                 fclose(fp);
                 return false;
             }
@@ -63,7 +66,8 @@ bool check_matrix(const char* filename)
     long curr_pos = ftell(fp);
     fseek(fp, 0, SEEK_END);
     long end_pos = ftell(fp);
-    if (end_pos != curr_pos) { // +1 потому что после каждой строки матрицы есть символ новой строки
+    if (end_pos != curr_pos)
+    {
         printf("Adjacency matrix is not square\n");
         fclose(fp);
         return false;
@@ -74,14 +78,17 @@ bool check_matrix(const char* filename)
 
 bool check_matrix_element(int x, int row, int col)
 {
-    if (x < 0) {
+    if (x < 0)
+    {
         printf("Invalid value in adjacency matrix at position (%d, %d)\n", row, col);
         return false;
     }
-    else if (isalpha(x)) {
+    else if (isalpha(x))
+    {
         printf("Invalid character in adjacency matrix at position (%d, %d)\n", row, col);
         return false;
     }
     return true;
 }
+
 #endif
