71c643d229f5547d68ef30af4b9e3149d29155db
cybersanta <cybersanta9@ya.ru>
2023-05-23 15:31:52 +0700

    Refactored main file

--- src/main.c
+++ src/main.c
@@ -1,64 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
+#include "filereader.h"
 #include "graphs.h"
 #include "stacknode.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    int graphSize = 12;
-    Edge edges[] = {
-        {7, 6, 3},
-        {6, 7, 5},
+    int graphSize, edgesSize;
+    int **matrix;
+    int vertexStart, vertexEnd;
+    bool exit = false;
 
-        {7, 2, 2},
-        {2, 7, 1},
+    if (argv[1])
+        matrix = readMatrixFile(argv[1], &graphSize);
+    else
+        matrix = readMatrixFile("./data/graph1.txt", &graphSize);
 
-        {7, 5, 3},
-        {5, 7, 7},
-
-        {6, 4, 4},
-        {4, 6, 8},
-
-        {2, 1, 9},
-        {1, 2, 11},
-
-        {5, 9, 3},
-        {9, 5, 1},
-
-        {4, 10, 2},
-        {10, 4, 3},
-
-        {1, 10, 5},
-        {10, 1, 3},
-
-        {9, 10, 4},
-        {10, 9, 7},
-    };
-
-    int n = sizeof(edges) / sizeof(edges[0]);
-
-    struct Graph *graph = createGraph(edges, n, graphSize);
+    Edge *edges = matrixToEdges(matrix, graphSize, &edgesSize);
+    Graph *graph = createGraph(edges, edgesSize, graphSize);
     printGraph(graph, graphSize);
 
-    StackNode *paths = getPathAllWrap(graph, 4, 7);
-    printf("Detected paths:\n");
-    printPaths(paths);
-    printf("\n");
+    char buff[100];
 
-    printf("Min paths:\n");
-    StackNode *minPath = findMinPath(graph, paths);
-    printPath(minPath);
-    printf("Weight: %d\n", calculateWeightForPath(graph, minPath));
-    printf("\n");
+    while (exit == false)
+    {
+        printf("Write vertex: <a> <b> to get info, or q to exit\n");
+        while (fgets(buff, 100, stdin) != NULL)
+        {
+            if (strstr(buff, "q"))
+            {
+                exit = true;
+                break;
+            }
+            sscanf(buff, "%d %d", &vertexStart, &vertexEnd);
 
-    printf("Max paths:\n");
-    StackNode *maxPath = findMaxPath(graph, paths);
-    printPath(maxPath);
-    printf("Weight: %d\n", calculateWeightForPath(graph, maxPath));
+            if((vertexStart < 0 || vertexStart > graphSize) || (vertexEnd < 0 || vertexEnd > graphSize) || (vertexStart == vertexEnd))
+            {
+                printf("Written data is not valid\n\n");
+                break;
+            }
+            printf("\nSUMMARY INFO\n");
+            StackNode *paths = getPathAllWrap(graph, vertexStart, vertexEnd);
+            printf("Detected paths:\n");
+            printPaths(paths);
+            printf("\n");
 
-    freePaths(paths);
+            printf("Min paths:\n");
+            StackNode *minPath = findMinPath(graph, paths);
+            printPath(minPath);
+            printf("Weight: %d\n", calculateWeightForPath(graph, minPath));
+            printf("\n");
+
+            printf("Max paths:\n");
+            StackNode *maxPath = findMaxPath(graph, paths);
+            printPath(maxPath);
+            printf("Weight: %d\n", calculateWeightForPath(graph, maxPath));
+
+            freePaths(paths);
+            vertexStart = -1;
+            vertexEnd = -1;
+            printf("\n");
+            break;
+        }
+    }
 
     return 0;
 }
